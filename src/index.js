32ae2c1a0792b3c9e9d55016f301e5bddd9564a1
Lucas <kjk727@me.com>
2019-02-14 15:06:35 +0900

    ApolloServer + Koa 예제 테스트

--- src/index.js
+++ src/index.js
@@ -1,52 +1,28 @@
-const { ApolloServer, gql } = require("apollo-server");
+const Koa = require("koa");
+const { ApolloServer, gql } = require("apollo-server-koa");
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-	{
-		title: "Harry Potter and the Chamber of Secrets",
-		author: "J.K. Rowling"
-	},
-	{
-		title: "Jurassic Park",
-		author: "Michael Crichton"
-	}
-];
+const port = 4000;
+const host = "localhost";
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
+// Construct a schema, using GraphQL schema language
 const typeDefs = gql`
-	# Comments in GraphQL are defined with the hash (#) symbol.
-
-	# This "Book" type can be used in other type declarations.
-	type Book {
-		title: String
-		author: String
-	}
-
-	# The "Query" type is the root of all GraphQL queries.
-	# (A "Mutation" type will be covered later on.)
 	type Query {
-		books: [Book]
+		hello: String
 	}
 `;
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
+// Provide resolver functions for your schema fields
 const resolvers = {
 	Query: {
-		books: () => books
+		hello: () => "Hello world!"
 	}
 };
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-	console.log(`🚀  Server ready at ${url}`);
-});
+const app = new Koa();
+apolloServer.applyMiddleware({ app });
+
+app.listen(port, host, () =>
+	console.log(`🚀 Server ready at http://${host}:${port}${server.graphqlPath}`)
+);
